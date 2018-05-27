4094be2c13225eaa4381ae6c313455a7129dca9f
blaze6950 <d.progect21@gmail.com>
2018-05-27 13:48:55 +0300

    Update: - some bug fixes

--- TVManager(WPF, ADONet)/Presenters/TVSeriesWindowPresenter.cs
+++ TVManager(WPF, ADONet)/Presenters/TVSeriesWindowPresenter.cs
@@ -78,7 +78,10 @@ namespace TVManager_WPF__ADONet_.Presenters
             LoadChannels();
             LoadGenres();
             _view.Description.Text = _TVSeriesExtended.Description;
-            _view.ImageTVSeries.Source = new BitmapImage(new Uri(_TVSeriesExtended.Image));
+            if (_TVSeriesExtended.Image.Length > 0)
+            {
+                _view.ImageTVSeries.Source = new BitmapImage(new Uri(_TVSeriesExtended.Image));
+            }
             _view.Year.Text = _TVSeriesExtended.Year.ToString();
             _view.NumberOfSeasons.Text = _TVSeriesExtended.NumberOfSeasons.ToString();
         }
@@ -127,9 +130,40 @@ namespace TVManager_WPF__ADONet_.Presenters
                     AddNewTVSeries();
                     break;
                 case TVSeriesWindowMode.Edit:
-                    InitializeViewModeEdit();
+                    UpdateTVSeries();
                     break;
             }
+        }
+
+        void UpdateTVSeries()
+        {
+            //_TVSeriesExtended = new TVSeriesExtended();
+            _TVSeriesExtended.Name = _view.NameTVSeries.Text;
+            if ((_view.ImageTVSeries.Source as BitmapImage) != null)
+            {
+                _TVSeriesExtended.Image = (_view.ImageTVSeries.Source as BitmapImage).UriSource.AbsolutePath;
+            }
+            if (Int32.TryParse(_view.Year.Text, out var buf))
+            {
+                _TVSeriesExtended.Year = buf;
+            }
+            if (Int32.TryParse(_view.NumberOfSeasons.Text, out buf))
+            {
+                _TVSeriesExtended.NumberOfSeasons = buf;
+            }
+            _TVSeriesExtended.Description = _view.Description.Text;
+            _TVSeriesExtended.Channel = _view.Channel.Text;
+            _TVSeriesExtended.GenreList.Clear();
+            foreach (object genre in _view.Genre.Items)
+            {
+                if (genre is CheckBox && ((CheckBox)genre).IsChecked == true)
+                {
+                    _TVSeriesExtended.GenreList.Add((String)((CheckBox)genre).Content);
+                }
+            }
+            _model.RemoveTvSeriesItem(_TVSeriesExtended);
+            _model.AddTVSeriesItem(_TVSeriesExtended);
+            _view.CloseWindow();
         }
 
         private void AddNewTVSeries()
@@ -163,7 +197,7 @@ namespace TVManager_WPF__ADONet_.Presenters
 
         public void CancelButtonClick()
         {
-            throw new NotImplementedException();
+            _view.CloseWindow();
         }
     }
 }
